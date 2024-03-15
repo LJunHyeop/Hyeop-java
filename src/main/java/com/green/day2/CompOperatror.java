72b1bde6bdeaef5313795d4f8378d4e0433353bf
Administrator <dlwnsguq2002@gmail.com>
2024-03-15 12:34:44 +0900

    first

---
+++ src/main/java/com/green/day2/CompOperatror.java
@@ -0,0 +1,16 @@
+package com.green.day2;
+//비교 연산자 누가누가 더큰지 정수혀
+public class CompOperatror {
+    public static void main(String[] args){
+    boolean b1 = true;
+    boolean b2 = false;
+    // boolean b3 - 32;
+        boolean r1 = 10 > 9; // 10이 9보다  초과 인지 물어보는것
+        System.out.println(r1);
+        r1 = 10 < 9; // 10이 9보다 미만 인지 물어보는것
+        System.out.println(r1);
+        r1 = 10 >= 9;
+        System.out.println(r1);
+
+    }
+}
